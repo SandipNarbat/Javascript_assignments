5105b3a1d24162fb4c7fb99154cb3c142ebb7a38
Sandip Narbat <Sandipnarbat111@gmail.com>
2025-09-09 19:06:07 +0530

    assignment 2 uploaded

---
+++ assignment 2/main.js
@@ -0,0 +1,4 @@
+let name = prompt("Enter your name:");
+let age = parseInt(prompt("Enter your age:"));
+let futureAge = age + 5;
+alert("Hello, " + name + " In 5 years, you will be " + futureAge + " years old.");
